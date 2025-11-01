9387b0838b1d9cac875c1ba4c118f7d04ddbf7df
root <root@vm123.simplyblock5.localdomain>
2025-11-01 15:19:10 +0100

    remove upd port block

--- lib/lvol/lvol.c
+++ lib/lvol/lvol.c
@@ -2994,14 +2994,12 @@ static void
 block_port(int port) {
     // Construct the iptables command dynamically based on the input port    
 	if (port != 0) {
-		char command[700];
+		char command[500];
 		// snprintf(command, sizeof(command), "sudo iptables -A INPUT -p tcp --dport %d -j DROP && sudo iptables -A OUTPUT -p tcp --dport %d -j DROP", port, port);
 		snprintf(command, sizeof(command),
     		"sudo iptables -C INPUT -p tcp --dport %d -j DROP 2>/dev/null || sudo iptables -A INPUT -p tcp --dport %d -j DROP;"
-    		"sudo iptables -C OUTPUT -p tcp --dport %d -j DROP 2>/dev/null || sudo iptables -A OUTPUT -p tcp --dport %d -j DROP;"
-			"sudo iptables -C INPUT -p udp --dport %d -j DROP 2>/dev/null || sudo iptables -A INPUT -p udp --dport %d -j DROP;"
-			"sudo iptables -C OUTPUT -p udp --dport %d -j DROP 2>/dev/null || sudo iptables -A OUTPUT -p udp --dport %d -j DROP;",
-    		port, port, port, port, port, port, port, port);
+    		"sudo iptables -C OUTPUT -p tcp --dport %d -j DROP 2>/dev/null || sudo iptables -A OUTPUT -p tcp --dport %d -j DROP;",
+    		port, port, port, port);
 
 		// Execute the command
 		int result = system(command);
