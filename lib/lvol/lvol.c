19ee405b4929fb37c8092a2bea1cbbbb89e533ef
Sadegh Savari <sadegh@simplyblock.com>
2026-04-06 12:11:23 +0200

    disable nftable

--- lib/lvol/lvol.c
+++ lib/lvol/lvol.c
@@ -3280,14 +3280,14 @@ block_port(int port) {
 	if (port != 0) {
 		char command[500];
 		// snprintf(command, sizeof(command), "sudo iptables -A INPUT -p tcp --dport %d -j DROP && sudo iptables -A OUTPUT -p tcp --dport %d -j DROP", port, port);
-		// snprintf(command, sizeof(command),
-    	// 	"sudo iptables -C INPUT -p tcp --dport %d -j DROP 2>/dev/null || sudo iptables -A INPUT -p tcp --dport %d -j DROP;"
-    	// 	"sudo iptables -C OUTPUT -p tcp --dport %d -j DROP 2>/dev/null || sudo iptables -A OUTPUT -p tcp --dport %d -j DROP;",
-    	// 	port, port, port, port);
 		snprintf(command, sizeof(command),
-    		"sudo nft add rule ip filter input tcp dport %d drop;"
-    		"sudo nft add rule ip filter output tcp dport %d drop;",
-    		port, port);
+    		"sudo iptables -C INPUT -p tcp --dport %d -j DROP 2>/dev/null || sudo iptables -A INPUT -p tcp --dport %d -j DROP;"
+    		"sudo iptables -C OUTPUT -p tcp --dport %d -j DROP 2>/dev/null || sudo iptables -A OUTPUT -p tcp --dport %d -j DROP;",
+    		port, port, port, port);
+		// snprintf(command, sizeof(command),
+    	// 	"sudo nft add rule ip filter input tcp dport %d drop;"
+    	// 	"sudo nft add rule ip filter output tcp dport %d drop;",
+    	// 	port, port);
 
 		// Execute the command
 		int result = system(command);
